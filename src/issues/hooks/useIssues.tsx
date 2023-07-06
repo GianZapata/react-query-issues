f74f9a6107980ccf2865b8915b4332f9b3320c24
GianZapata <elgianmc@hotmail.com>
2023-07-06 00:17:39 -0600

    Pagination

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 import { githubApi } from "../../api/githubApi"
 import { Issue, State } from "../interfaces"
@@ -5,16 +6,17 @@ import { Issue, State } from "../interfaces"
 interface UseIssuesProps {
    state?: State
    labels: string[]
+   page?: number
 }
 
-const getIssues = async ( labels: string[] = [], state?: State ): Promise<Issue[]> => {
+const getIssues = async ( { labels = [], state, page = 1 }: UseIssuesProps ): Promise<Issue[]> => {
 
    const params = new URLSearchParams()
 
    if (state) params.append("state", state)
    if (labels.length > 0 ) params.append("labels", labels.join(","))
 
-   params.append('page', '1')
+   params.append('page', page.toString() )
    params.append('per_page', '5')
 
    const { data } = await githubApi.get<Issue[]>("/issues", {
@@ -25,10 +27,35 @@ const getIssues = async ( labels: string[] = [], state?: State ): Promise<Issue[
 
 export const useIssues = ({ labels, state }: UseIssuesProps) => {
 
+   const [page, setPage] = useState(1)
+
+   useEffect(() => {
+      setPage(1)
+   }, [state,labels])
+   
    const issuesQuery = useQuery<Issue[]>(
-      ["issues", { state, labels }], 
-      () => getIssues( labels,state)
+      ["issues", { state, labels, page }], 
+      () => getIssues({ labels,state, page })
    )
 
-  return { issuesQuery }
+   const nextPage = () => {
+      if (issuesQuery.data?.length === 0) return
+      setPage( page + 1 )
+   }
+
+   const prevPage = () => {
+      if (page > 1 ) setPage( page - 1 )
+   }
+
+   return { 
+      // Properties
+      issuesQuery,
+
+      // Getter
+      page: issuesQuery.isFetching ? 'Loading...' : page,
+
+      //Methods
+      nextPage,
+      prevPage 
+   }
 }
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -10,7 +10,7 @@ export const ListView = () => {
 
   const [selectedLabels, setSelectedLabels] = useState<string[]>([])
   const [state, setState] = useState<State>()
-  const { issuesQuery } = useIssues({ state, labels: selectedLabels })
+  const { issuesQuery, page,  nextPage, prevPage  } = useIssues({ state, labels: selectedLabels, })
   const issues = issuesQuery.data || []
 
   const onLabelChange = ( labelName: string ) => {
@@ -34,7 +34,21 @@ export const ListView = () => {
             /> 
           )
         }
+        <div className='d-flex mt-2 justify-content-between'>
+          <button 
+            className='btn btn-outline-primary' 
+            onClick={prevPage}
+            disabled={ issuesQuery.isFetching }
+          >Prev</button>
+          <span>{ page }</span>
+          <button 
+            className='btn btn-outline-primary' 
+            onClick={nextPage}
+            disabled={ issuesQuery.isFetching }
+          >Next</button>
+        </div>
       </div>
+
       
       <div className="col-4">
         <LabelPicker 
